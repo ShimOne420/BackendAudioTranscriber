83aa885bdae3e7eb0887c977e5295fb2e28ebbea
ShimOne420 <148460327+ShimOne420@users.noreply.github.com>
2025-02-22 18:32:20 +0100

    accesso ai due domini del frontend

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # ✅ Configura CORS per permettere l'accesso dal frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://frontend-eight-puce-41.vercel.app", "https://audiototext.duckdns.org"] 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
