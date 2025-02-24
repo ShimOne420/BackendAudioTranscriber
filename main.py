13ff06a0dcdb3aa5040da81eca089641ca0f196e
ShimOne420 <148460327+ShimOne420@users.noreply.github.com>
2025-02-24 22:09:48 +0100

    timeout più lungo

--- main.py
+++ main.py
@@ -157,4 +157,4 @@ def get_transcription(filename: str):
     
 # ✅ Avvia il server FastAPI sulla VM (porta 8000)
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=6000)
